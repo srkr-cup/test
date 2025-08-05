d3800e6dc275f01b67da033c15a9a6879d689528
srkr-cup <srkrcup@gmail.com>
2025-08-05 18:08:51 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
 
-export default function Home() {
-  const router = useRouter();
+// export default function Home() {
+//   const router = useRouter();
 
-  useEffect(() => {
-    // Redirect to the main page
-    router.push('/index.html');
-  }, []);
+//   useEffect(() => {
+//     // Redirect to the main page
+//     router.push('/index.html');
+//   }, []);
 
-  return null;
-}
+//   return null;
+// }
